e2ced3e591b030b1cdb3eb0c497d89a16048740c
Ryan Crosier <crosier.17@osu.edu>
2025-02-01 10:34:24 -0800

    autoloading

--- src/fap/main.py
+++ src/fap/main.py
@@ -1,8 +1,25 @@
+import os
+import yaml
 from fastapi import FastAPI
+from utils.constellation import Constellation
+
+DATA_DIR = "data/"
 
 app = FastAPI()
+
+sky = []
+for fname in os.listdir(DATA_DIR):
+	with open(DATA_DIR+fname) as f:
+		yml = yaml.safe_load(f)
+		con = Constellation(fname, yml)
+		sky.append(con)
 
 @app.get("/")
 async def root():
     return "<h1> gay<h1>"
+
+@app.post("/search_const")
+async def search():
+	return sky
+	
 
--- src/utils/loader.py
+++
@@ -1,7 +0,0 @@
-import yaml
-from constellation import Constellation
-
-with open("data/sample.yml") as f:
-    y = yaml.safe_load(f)
-
-c = Constellation("name", y["my_constellation"])
